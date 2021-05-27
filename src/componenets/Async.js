8f7b8c177ceffacff71d4f69902ccca4a02c8621
Miraj Hamid <miraj.hamid@axiatadigitallabs.com>
2021-05-27 15:32:12 +0530

    update for fetch method

--- src/componenets/Async.js
+++ src/componenets/Async.js
@@ -5,36 +5,70 @@ function Async(props) {
   //here we create a useState for data we going to fetch
   //and made it null since no data at first.
   const [gitHubData, setGitHubData] = useState(null);
+  const [loadingGitHubData, setLoadingGitHubData] = useState(false);
+  const [errorGitHubData, setErrorGitHubData] = useState(null);
+
+  //or you can use useEffects to just loadData with [] to load async data
+  // useEffect(() => {
+  //   fetch(`https://api.example.com/users/${login}`)
+  //     .then((response) => response.json())
+  //     .then(gitHubData);
+  // }, []);
 
   const apiFunction = (userName) => {
-    fetch(`https://api.github.com/users/${userName}`)
-      .then((response) => response.json())
-      .then(function (data) {
-        console.log(data);
-        //have to return something then only the "then"
-        //next can read it from there
-        return data;
-      })
-      .catch(function (err) {
-        console.log(err);
-      })
-      .then(function (data) {
-        setGitHubData(data);
-      });
+    setLoadingGitHubData(true);
+
+    if (userName.length > 3) {
+      fetch(`https://api.github.com/users/${userName}`)
+        .then((response) => response.json())
+        .then(function (data) {
+          console.log(data);
+          //have to return something then only the "then"
+          //next can read it from there
+          setLoadingGitHubData(false);
+          return data;
+        })
+        .catch(function (err) {
+          console.log(err);
+          setErrorGitHubData(err);
+        })
+        .then(function (data) {
+          setGitHubData(data);
+        });
+    } else if (!userName) {
+      setLoadingGitHubData(false);
+      setGitHubData(null);
+    } else {
+    }
   };
 
-  return (
-    <div className="preStyle">
-      <label>GitHub user name :</label>
-      <input
-        type="text"
-        id="username"
-        placeholder="type github username here"
-        onChange={(e) => apiFunction(e.target.value)}
-      />
-      <pre>{JSON.stringify(gitHubData, null, 10)}</pre>
-    </div>
-  );
+  if (loadingGitHubData) {
+    return (
+      <div className="preStyle">
+        <label>Country name :</label>
+        <input
+          type="text"
+          id="username"
+          placeholder="type github username here"
+          onChange={(e) => apiFunction(e.target.value)}
+        />
+        <pre>Loading...! userName should be length 3 or higher</pre>
+      </div>
+    );
+  } else {
+    return (
+      <div className="preStyle">
+        <label>Country name :</label>
+        <input
+          type="text"
+          id="username"
+          placeholder="type github username here"
+          onChange={(e) => apiFunction(e.target.value)}
+        />
+        <pre>{JSON.stringify(gitHubData, null, 10)}</pre>
+      </div>
+    );
+  }
 }
 
 export default Async;
